f9eaf2eb77c3f17b44f3d9d1f30af6fc12bd539e
smartpro1 <akeni.promise@yahoo.com>
2020-07-11 23:12:35 +0100

    create profile page

--- banka-ui/src/components/UserProfile/UserProfile.js
+++ banka-ui/src/components/UserProfile/UserProfile.js
@@ -1,21 +1,36 @@
 import React, { Component } from "react";
 
+import img1 from "../../assets/images/img1.PNG";
+import "./UserProfile.css";
 class UserProfile extends Component {
   render() {
     return (
       <div className="userProfile">
+        <div className="signup-logo">
+          <h3>
+            <a href="/dashboard">
+              Banka&nbsp;
+              <i className="fa fa-university logo-icon" aria-hidden="true"></i>
+            </a>
+          </h3>
+        </div>
+        <h3 className="dummy-accounts-h3">Profile Page</h3>
         <div className="userProfile-img">
-          <img src="" alt="profile image" />
+          <img src={img1} alt="profile image" id="profile-image" />
         </div>
         <div className="userProfile-details">
           <p>
-            <strong>Name:</strong>{" "}
+            <strong>Name</strong> : Akeni Promise
           </p>
           <p>
-            <strong>Email:</strong>{" "}
+            <strong>Email</strong> : ***ni.prom***@yahoo.com
           </p>
           <p>
-            <strong>Phone: </strong>
+            <strong>Phone</strong>: **0629313**
+          </p>
+
+          <p>
+            <strong>Username</strong>: **ar*
           </p>
         </div>
         <form className="userProfile-form"></form>
